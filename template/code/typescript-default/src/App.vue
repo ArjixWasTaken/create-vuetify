8f3b3ba08a553cb5c7ca9dfe4306adf2713563ad
ArjixWasTaken <53124886+ArjixWasTaken@users.noreply.github.com>
2023-08-12 16:43:11 +0000

    Update dist to output generated at 5d330d7

--- template/code/typescript-default/src/App.vue
+++ template/code/typescript-default/src/App.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts">
+import { usePreferredColorScheme } from '@vueuse/core'
+
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+const preferredColorScheme = usePreferredColorScheme()
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <v-theme-provider theme="preferredColorScheme">
+    <header>
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+      </div>
+    </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+      <TheWelcome />
+    </main>
+  </v-theme-provider>
 </template>
 
 <style scoped>
